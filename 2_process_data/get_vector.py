261077d5c542301d8b157df4ea921d2e834fa2e4
pkulics <lics999@126.com>
2019-09-10 11:32:41 +0800

    update

--- 2_process_data/get_vector.py
+++ 2_process_data/get_vector.py
@@ -1,18 +1,16 @@
-
+#encoding=utf-8
 from gensim.models import word2vec
+import jieba
 
-#file_dir = "pingjia/train.txt"
-#vec_dir = "pingjia/vec.bin"
-file_dir = "yt/train.txt"
-vec_dir = "yt/vec.bin"
+file_dir = "data/news.txt"
+vec_dir = "data/vec.bin"
 sentences = []
 with open(file_dir,'r',encoding='utf-8') as corpus:
     for line in corpus:
-        line = line.split('\t')[1]
-        line = line.split()
+        line = line.strip().strip("\n")
+        line = jieba.lcut(line)
         sentences.append(line)
-#sentences = word2vec.Text8corpus(file_dir)
-model = word2vec.Word2Vec(sentences, sg=1, size=300, window=5, min_count=5, negative=3,sample=0.001,hs=1,workers=4)
+model = word2vec.Word2Vec(sentences, sg=1, size=10, window=5, min_count=1, negative=3,sample=0.001,hs=1,workers=4)
 model.save(vec_dir)
-print(model['贵'])
+print("首富:",model['首富'])
 
